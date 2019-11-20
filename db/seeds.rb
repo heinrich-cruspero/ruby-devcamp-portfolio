fad1f44186eaf998a4e87420e4233ee17680d3d8
Marc Heinrich Cruspero <heinrich.cruspero@gmail.com>
2019-11-20 19:46:24 +0800

    Add portfolios controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+5.times do |blog|
+    Blog.create!(
+        title: "Blog Entry ##{blog}",
+        body: "Bacon ipsum dolor amet salami pork belly alcatra drumstick kevin flank chuck bacon andouille pig spare ribs t-bone pancetta beef picanha. Bacon beef ribs pig buffalo jowl salami doner meatloaf brisket andouille capicola ham hock chuck ball tip pancetta. Alcatra fatback pork chop chuck spare ribs kevin jerky bresaola sausage meatball beef ribs andouille pastrami short ribs. Pastrami flank bresaola chuck kevin beef ribs tenderloin, capicola chislic fatback t-bone",
+    )
+end
+
+10.times do |portfolio|
+    Portfolio.create!(
+        title: "Portfolio title ##{portfolio}",
+        subtitle: "Subtitle ##{portfolio}",
+        body: "Bacon ipsum dolor amet salami pork belly alcatra drumstick kevin flank chuck bacon andouille pig spare ribs t-bone pancetta beef picanha. Bacon beef ribs pig buffalo jowl salami doner meatloaf brisket andouille capicola ham hock chuck ball tip pancetta. Alcatra fatback pork chop chuck spare ribs kevin jerky bresaola sausage meatball beef ribs andouille pastrami short ribs. Pastrami flank bresaola chuck kevin beef ribs tenderloin, capicola chislic fatback t-bone",
+        main_image: "https://picsum.photos/id/#{rand(1..200)}/600/300",
+        thumb_image: "https://picsum.photos/id/#{rand(1..200)}/200/300",
+    )
+end
+
