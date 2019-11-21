14af6d40b2b8e289d1e61fb48f18571c7dde31ea
Marc Heinrich Cruspero <heinrich.cruspero@gmail.com>
2019-11-22 01:11:29 +0800

    Add custom method

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  # :variable means `url` also means symbolvariable while variable: means dict
   # customize resources
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: 'show_portfolio'
 
-  resources :blogs
+  resources :blogs do
+    member do
+      get :toggle_status, as: 'toggle_status'
+    end
+  end
 
   # Static Page == get `/url/` to views (app#suffix)
   get 'about', to: 'pages#about'
