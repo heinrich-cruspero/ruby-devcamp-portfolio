ef06101e8f3c5db533637bf30bb1d8611a613bb1
Marc Heinrich Cruspero <heinrich.cruspero@gmail.com>
2019-11-24 17:10:52 +0800

    Add concern for devise whitelist

---
+++ app/controllers/concerns/devise_whitelist.rb
@@ -0,0 +1,14 @@
+# UpperCamelCase. Capital are mapped with filename '_'
+module DeviseWhitelist
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :configure_permitted_params, if: :devise_controller?
+  end
+
+  def configure_permitted_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+
+end
