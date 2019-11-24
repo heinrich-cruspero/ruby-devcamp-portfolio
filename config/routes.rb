f51a80e6eccec9bb6ef7b45c3702e4452f3e70e5
Marc Heinrich Cruspero <heinrich.cruspero@gmail.com>
2019-11-24 16:28:17 +0800

    Add devise feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # :variable means `url` also means symbolvariable while variable: means dict
   # customize resources
   resources :portfolios, except: [:show]
