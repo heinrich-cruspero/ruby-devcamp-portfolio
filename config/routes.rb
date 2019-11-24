b82b590b885a91ac7de7afb7d662553b38de56b2
Marc Heinrich Cruspero <heinrich.cruspero@gmail.com>
2019-11-24 16:34:29 +0800

    Add custom devise urls

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   # :variable means `url` also means symbolvariable while variable: means dict
   # customize resources
   resources :portfolios, except: [:show]
